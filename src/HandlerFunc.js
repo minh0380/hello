93e5a34537a49c2c0ca89c5c777c80789e839417
minh0380 <minh0380@naver.com>
2021-03-08 15:34:13 +0900

    react Props

--- src/HandlerFunc.js
+++ src/HandlerFunc.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import List from './study';
+//import List from './study';
 
 const HandlerFunc = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ]
+
     const handleChange = event => {
         console.log(event.target.value);
     }
+
+    const List = props =>
+        props.list.map(item => (
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+        ))
 
     return(
         <div>
@@ -12,7 +43,7 @@ const HandlerFunc = () => {
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" onChange={handleChange}/>
             <hr/>
-            <List/>
+            <List list={stories}/>
         </div>
     )
 }
